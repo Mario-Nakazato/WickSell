ae4a2233ddb9894f87696ea7c2af67fe91d709da
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-04-30 18:58:32 -0300

    Integração (FrontEnd && back-end) -> main

--- components/AccountInfo.tsx
+++ components/AccountInfo.tsx
@@ -1,19 +1,30 @@
-export default function AccountInfo(): JSX.Element {
-    var accountInfoLogged = false;
-    if (!accountInfoLogged) {
-        return (<>
-            <div id="accountInfo">
-                <div id="account-btn">
-                    <a id="login-btn" onClick={() => { }}>Entrar</a>
-                    <div id="btn-separator" >|</div>
-                    <a id="register-btn" onClick={() => { }}>Registrar-se</a>
-                </div>
-                <div>
-                    <img id="profile-img" src="/profile-placeholder.png"></img>
-                </div>
+import { NextComponentType } from "next";
+import { useSession, signIn, signOut } from "next-auth/react"
+
+const AccountInfo: NextComponentType = () => {
+    const { data: session } = useSession()
+    if (session) {
+        return (
+            <div>
+                Conectado como {session?.user?.email} <br /><br />
+                <button onClick={() => signOut()}>Sair </button>|{" "}
+                <button onClick={() => signIn("auth0", null!, { prompt: "login" })}>Usar outra conta</button><br />
+                <button>Perfil</button><br />
             </div>
-        </>)
-    } else {
-        return <></>
+        )
     }
+    return (<>
+        <div id="accountInfo">
+            <div id="account-btn">
+                <a id="login-btn" onClick={() => signIn("auth0")}>Entrar</a>
+                <div id="btn-separator" >|</div>
+                <a id="register-btn" onClick={() => signIn("auth0")}>Registrar-se</a>
+            </div>
+            <div>
+                <img id="profile-img" src="/profile-placeholder.png"></img>
+            </div>
+        </div>
+    </>)
+
 }
+export default AccountInfo
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,58 @@
 import type { NextPage } from 'next'
+import { useSession } from 'next-auth/react'
 import DefaultHead from '../components/DefaultHead'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <DefaultHead></DefaultHead>
-      <Header></Header>
-      <h1>Hello world</h1>
-      <Footer></Footer>
-    </>
-  )
+  const { data: session, status } = useSession()
+
+  if (session && status == "authenticated") {
+    return (
+      <>
+        <DefaultHead></DefaultHead>
+        <body>
+          <Header></Header>
+          <h1>Produtos perfil [Promoção, similares, novidades ...]</h1>
+          <ul>
+            <li><a href='./auth'>Produto exemplo</a></li>
+            <li>Produto 1</li>
+            <li>Produto 2</li>
+            <li>Produto 3</li>
+            <li>Produto 4</li>
+          </ul>
+        </body>
+      </>
+    )
+  } else if (status == "loading") {
+    return (
+      <>
+        <DefaultHead></DefaultHead>
+        <body>
+
+          <Header></Header>
+          <h1>Carregando...</h1>
+        </body>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <DefaultHead></DefaultHead>
+        <body>
+          <Header></Header>
+          <h1>Produtos geral [Estrategias mais comprado, promoção ...]</h1>
+          <ul>
+            <li><a href='./auth'>Produto exemplo</a></li>
+            <li>Produto 1</li>
+            <li>Produto 2</li>
+            <li>Produto 3</li>
+            <li>Produto 4</li>
+          </ul>
+        </body>
+      </>
+    )
+  }
 }
 
 export default Home
