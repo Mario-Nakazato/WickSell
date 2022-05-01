e85c2f5cb76b323f297bbc223849b3040251ed7c
Mario Nakazato <marionakazat@gmail.com>
2022-05-01 14:04:09 -0300

    refatorado api perfil

--- pages/api/perfil.tsx
+++ pages/api/perfil.tsx
@@ -3,8 +3,9 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from "next-auth/react"
 import perfil from "../../utils/bdperfil"
 
+const cperfil = new perfil(process.env.MONGODB_COLECAO_PERFILAUTH0!)
+
 export default async function apiPerfil(req: NextApiRequest, res: NextApiResponse) {
-  const cperfil = new perfil(process.env.MONGODB_COLECAO_PERFILAUTH0!)
   const session = await getSession({ req })
   if (session) {
     let profile: any = { sub: session?.user?.email }
@@ -12,7 +13,8 @@ export default async function apiPerfil(req: NextApiRequest, res: NextApiRespons
     profile = { email: usuario?.email }
     usuario = await cperfil.findAllProfile(profile)
 
-    return res.status(200).json(usuario)
+    res.status(200).json(usuario)
+  } else {
+    res.status(400).json({ txt: "404" })
   }
-  res.status(400).json({txt: "404"})
 }
