c599b1682a230702b1629636d882ab49e221e7fe
Mario Nakazato <marionakazat@gmail.com>
2022-04-30 22:03:19 -0300

    correção do nome no title

--- components/DefaultHead.tsx
+++ components/DefaultHead.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 export default function DefaultHead() {
     return (
         <Head>
-            <title>SellWick E-Commerce</title>
+            <title>WickSell E-Commerce</title>
             <link rel="icon" href="/favicon.ico" />
         </Head>
     )
