bb7bc5fc6913a0aa5f7539c1bfa555a9525977e4
Mario Nakazato <marionakazat@gmail.com>
2022-04-30 21:46:55 -0300

    Alteração entrar na conta

--- components/AccountInfo.tsx
+++ components/AccountInfo.tsx
@@ -1,30 +1,33 @@
 import { NextComponentType } from "next";
 import { useSession, signIn, signOut } from "next-auth/react"
 
-const AccountInfo: NextComponentType = () => {
-    const { data: session } = useSession()
-    if (session) {
+const AccountInfo: NextComponentType | any = () => {
+
+    const { data: session, status } = useSession()
+
+    if (status == "loading") return <>Loading...{session}</>;
+
+    if (status == "authenticated") {
         return (
-            <div>
-                Conectado como {session?.user?.email} <br /><br />
-                <button onClick={() => signOut()}>Sair </button>|{" "}
-                <button onClick={() => signIn("auth0", null!, { prompt: "login" })}>Usar outra conta</button><br />
-                <button>Perfil</button><br />
+            <>
+                Conectado como {session?.user?.email}<br />
+                <a onClick={() => signOut()}>Sair</a>
+                <a>Perfil</a>
+            </>
+        )
+    } else if (status == "unauthenticated") {
+        return (
+            <div id="accountInfo">
+                <div id="account-btn">
+                    <a id="login-btn" onClick={() => signIn("auth0", null!, { prompt: "login" })}>Entrar</a>
+                    <div id="btn-separator" >|</div>
+                    <a id="register-btn" onClick={() => signIn("auth0")}>DEBUG Entrar</a>
+                </div>
+                <div>
+                    <img id="profile-img" src="/profile-placeholder.png"></img>
+                </div>
             </div>
         )
     }
-    return (<>
-        <div id="accountInfo">
-            <div id="account-btn">
-                <a id="login-btn" onClick={() => signIn("auth0")}>Entrar</a>
-                <div id="btn-separator" >|</div>
-                <a id="register-btn" onClick={() => signIn("auth0")}>Registrar-se</a>
-            </div>
-            <div>
-                <img id="profile-img" src="/profile-placeholder.png"></img>
-            </div>
-        </div>
-    </>)
-
 }
 export default AccountInfo
