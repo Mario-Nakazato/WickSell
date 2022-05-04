e9b7d90323dd15793e9d2397b746093b259ea764
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-05-03 22:09:56 -0300

    <a>

--- components/EmailVerifyMessage.tsx
+++ components/EmailVerifyMessage.tsx
@@ -1,6 +1,6 @@
 import { NextComponentType } from "next";
 import { signIn } from "next-auth/react"
-import { resolveHref } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
 import styles from '../styles/EmailVerifyMessage.module.css'
 const EmailVerifyMessage: NextComponentType = () => {
     return (<>
@@ -8,7 +8,7 @@ const EmailVerifyMessage: NextComponentType = () => {
             <label className={styles.Label}>Seu email ainda não foi verificado.</label>
             <label className={styles.Label}>Por favor verifique-o, antes de entrar.</label>
             <div className={styles.LinkContainer}>
-                <a className={styles.Link}  href='/'>Continuar sem verificar</a>
+                <Link href='/'><a className={styles.Link} >Continuar sem verificar</a></Link>
                 <div className={styles.LinkSeparator}></div>
                 <div className={styles.Link} onClick={() => { signIn("auth0") }}>Já verifiquei o email</div>
             </div>
