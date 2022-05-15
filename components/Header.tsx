1ea17a5293b14b7e2ecb86f0e2ebbf76a0c189a2
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-05-15 01:16:00 -0300

    ATL

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,43 @@
 import AccountInfo from './AccountInfo'
 import styles from '../styles/Header.module.css'
-export default function Header() {
-    return (<>
-        <header className={styles.Header}>
-            <div className={styles.UpperSideHeader}>
-                <a className={styles.LogoA} href=""><img className={styles.LogoIcon} src="/logo.png" alt="Logo"></img></a>
-                <form action='/api/' className={styles.SearchContainer}>
-                    <input type="text" placeholder="Pesquisa" className={styles.SearchInput}></input>
-                    <button type='submit' className={styles.SearchBtn}><img className={styles.SearchIcon} src='/lupa-icon.svg' alt="Search Icon"></img></button>
-                </form>
-                <AccountInfo />
-            </div>
-            <div className={styles.NavBar}>
-                <div className={styles.NavBarTr}>
-                    <div className={styles.NavBarUl}>Item 1</div>
-                    <div className={styles.NavBarUl}>Item 2</div>
-                    <div className={styles.NavBarUl}>Item 3</div>
-                    <div className={styles.NavBarUl}>Item 4</div>
-                    <div className={styles.NavBarUl}>Item 5</div>
+export default function Header(props: any) {
+    if (props.type === 'Default' || props.type === undefined) {
+        return (<>
+            <header className={styles.Header}>
+                <div className={styles.UpperSideHeader}>
+                    <a className={styles.LogoA} href=""><img className={styles.LogoIcon} src="/logo.png" alt="Logo"></img></a>
+                    <form action='/api/' className={styles.SearchContainer}>
+                        <input type="text" placeholder="Pesquisa" className={styles.SearchInput}></input>
+                        <button type='submit' className={styles.SearchBtn}><img className={styles.SearchIcon} src='/lupa-icon.svg' alt="Search Icon"></img></button>
+                    </form>
+                    <AccountInfo />
                 </div>
-            </div>
-        </header>
-    </>)
+                <div className={styles.NavBar}>
+                    <div className={styles.NavBarTr}>
+                        <div className={styles.NavBarUl}>Item 1</div>
+                        <div className={styles.NavBarUl}>Item 2</div>
+                        <div className={styles.NavBarUl}>Item 3</div>
+                        <div className={styles.NavBarUl}>Item 4</div>
+                        <div className={styles.NavBarUl}>Item 5</div>
+                    </div>
+                </div>
+            </header>
+        </>)
 
+    }
+    if (props.type === 'Perfil') {
+        return (<>
+            <header className={styles.Header}>
+                <div className={styles.UpperSideHeader}>
+                    <a className={styles.LogoA} href=""><img className={styles.LogoIcon} src="/logo.png" alt="Logo"></img></a>
+                    <form action='/api/' className={styles.SearchContainer}>
+                        <input type="text" placeholder="Pesquisa" className={styles.SearchInput}></input>
+                        <button type='submit' className={styles.SearchBtn}><img className={styles.SearchIcon} src='/lupa-icon.svg' alt="Search Icon"></img></button>
+                    </form>
+                    <AccountInfo />
+                </div>
+            </header>
+        </>)
+    }
+    return <></>
 }
