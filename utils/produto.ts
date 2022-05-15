7866f2a1e31eafc0ba3518b33ea7c79c553c8e19
Mario Nakazato <marionakazat@gmail.com>
2022-05-15 18:01:15 -0300

    API produto pesquisa e melhorias

--- utils/produto.ts
+++ utils/produto.ts
@@ -7,8 +7,8 @@ const colecao = process.env.MONGODB_COLLECTION_PRODUTO!
 export default class Produto {
 
     private _id!: ObjectId
-    private name!: string;
-    private description!: string;
+    private name!: string | {};
+    private description!: string | {};
     private price!: number;
     private image!: string;
     private promotion!: number;
@@ -18,10 +18,10 @@ export default class Produto {
             this._id = new ObjectId(_id)
         }
         if (name != undefined) {
-            this.name = String(name)
+            this.name = { $regex: String(name), $options: 'i' }
         }
         if (description != undefined) {
-            this.description = String(description)
+            this.description = { $regex: String(description), $options: 'i' }
         }
         if (price != undefined) {
             this.price = Number(price)
@@ -32,6 +32,7 @@ export default class Produto {
         if (promotion != undefined) {
             this.promotion = Number(promotion)
         }
+        console.log(this)
     }
 
     async insertOne() {
