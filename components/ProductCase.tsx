3269cd9aac5fc2ab0e2d0a968244b619fa1c9a3d
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-05-15 16:35:18 -0300

    Ajustes no ProducCase

--- components/ProductCase.tsx
+++ components/ProductCase.tsx
@@ -10,16 +10,38 @@ type props = {
 }
 
 export default function ProductCase(props: any) {
-    if (!props.name) props = new Product('1', 'Geladeira FrostFree', 'Ela gela, confia...', 'R$ 4.999,90', 'R$ 6.999,90', '/geladeira.jpg')
+    //if (!props.name) props = new Product('1', 'Geladeira FrostFree', 'Ela gela, confia...', '4.999,90', '6.999,90', '/geladeira.jpg')
+    var promotion = ''
+    var price = ''
+    var link = ``
+    var linkAs = ``
+    if (props.promotion && props.promotion > 0) {
+        promotion = 'R$ ' + props.promotion
+    } else if (props.promotion && props.promotion == 0) {
+        promotion = ''
+    } else {
+        promotion = 'R$ 999,90'
+    }
+    if (props.price && props.promotion && props.promotion < props.price) {
+        price = 'R$ ' + props.promotion
+        promotion = 'R$ ' + props.price
+    } else {
+        price = 'R$ ' + (props.price || '3999,99')
+    }
+    if (props.id) {
+        link = `/product/[id]`
+        linkAs = `/product/${props.id}`
+    }
+
     return (<>
         <div className={styles.ProductCase}>
 
-            <Link href="/product/[id]" as={`/product/${props.id}`}>
+            <Link href={link} as={linkAs}>
                 <a>
-                    <img className={styles.ProductImage} src={props.image} alt={props.name}></img>
-                    <div className={styles.ProductName}>{props.name}</div>
-                    <div className={styles.ProductPromotion}>{props.promotion}</div>
-                    <div className={styles.ProductPrice}>{props.price}</div>
+                    <img className={styles.ProductImage} src={props.image || '/product-placeholder.png'} alt={props.name}></img>
+                    <div className={styles.ProductName}>{props.name || 'Nome do Produto'}</div>
+                    <div className={styles.ProductPromotion} >{promotion}</div>
+                    <div className={styles.ProductPrice}>{price}</div>
                     <div className={styles.ProductBuyButton}>Comprar</div>
                 </a>
             </Link>
