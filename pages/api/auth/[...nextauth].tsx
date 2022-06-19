1425f2580c917a6b590cf462dc04330304548a7b
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-06-18 21:32:29 -0300

    add Promise on auth

--- pages/api/auth/[...nextauth].tsx
+++ pages/api/auth/[...nextauth].tsx
@@ -6,46 +6,48 @@ import Auth from "../../../utils/auth"
 const auth = new Auth()
 
 export default async function NextA(req: NextApiRequest, res: NextApiResponse) {
-    NextAuth(req, res, {
+    return new Promise(async (resolve, reject) => {
+        NextAuth(req, res, {
 
-        providers: [
-            Auth0Provider({
-                clientId: process.env.AUTH0_CLIENT_ID!,
-                clientSecret: process.env.AUTH0_CLIENT_SECRET!,
-                issuer: process.env.AUTH0_ISSUER
-            })
-        ],
+            providers: [
+                Auth0Provider({
+                    clientId: process.env.AUTH0_CLIENT_ID!,
+                    clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+                    issuer: process.env.AUTH0_ISSUER
+                })
+            ],
 
-        secret: process.env.NEXTAUTH_SECRET,
+            secret: process.env.NEXTAUTH_SECRET,
 
-        callbacks: {
-            async signIn({ user, account, profile, email, credentials }) {
-                user.email = profile.sub // trocado para o sub<autenticador [google-oauth2, auth0]>
+            callbacks: {
+                async signIn({ user, account, profile, email, credentials }) {
+                    user.email = profile.sub // trocado para o sub<autenticador [google-oauth2, auth0]>
 
-                await auth.signInProfile(profile)
+                    await auth.signInProfile(profile)
 
-                if (!profile.email_verified) {
-                    return false
+                    if (!profile.email_verified) {
+                        return false
+                    }
+                    return true
+                },
+                async redirect({ url, baseUrl }) {
+                    return baseUrl
+                },
+                async session({ session, user, token }) {
+                    return session
+                },
+                async jwt({ token, user, account, profile, isNewUser }) {
+                    return token
                 }
-                return true
             },
-            async redirect({ url, baseUrl }) {
-                return baseUrl
-            },
-            async session({ session, user, token }) {
-                return session
-            },
-            async jwt({ token, user, account, profile, isNewUser }) {
-                return token
-            }
-        },
 
-        pages: {
-            //signIn: '/auth/signin',
-            //signOut: '/auth/signout',
-            error: '/', // Error code passed in query string as ?error=
-            //verifyRequest: '/auth/verify-request', // (used for check email message)
-            //newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-        },
+            pages: {
+                //signIn: '/auth/signin',
+                //signOut: '/auth/signout',
+                error: '/', // Error code passed in query string as ?error=
+                //verifyRequest: '/auth/verify-request', // (used for check email message)
+                //newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+            },
+        })
     })
 }
