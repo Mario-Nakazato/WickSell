ff2fd5537bdba77908e8c0dc18caae7d1a0341ba
Mario Nakazato <marionakazat@gmail.com>
2022-06-22 17:55:33 -0300

    Add a lista de execução em POST e DELETE do estoque

--- pages/api/produto.ts
+++ pages/api/produto.ts
@@ -1,7 +1,7 @@
-import multer from 'multer'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react'
 import Produto from "../../utils/produto"
+import Perfil from '../../utils/perfil'
 
 export default async function apiProduto(req: NextApiRequest, res: NextApiResponse) {
 
@@ -9,9 +9,9 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
 
     if (req.method == "GET") {
 
-        const { _id, name, description, price, discount } = req.query
+        const { _id, name, description, price, discount, amount } = req.query
         try {
-            produto.set(_id, name, description, price, null, discount)
+            produto.set(_id, name, description, price, null, discount, amount)
         } catch (e) {
             res.status(400).json({ txt: "_id invalido." })
             return
@@ -22,57 +22,68 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
     }
 
     const session = await getSession({ req })
-
-    if (!session && req.rawHeaders.filter((value) => { return value == "insomnia/2022.4.1" })[0] !== "insomnia/2022.4.1") {
+    if (!session && req.rawHeaders.filter((value) => { return value == "insomnia/2022.4.2" })[0] !== "insomnia/2022.4.2") {
         res.status(400).json({ txt: "Acesso negado." })
         return
     }
 
+    const perfil = new Perfil()
+	const email = await perfil.setEmail(session?.user?.email!)
+    const documentoPerfil = await perfil.findOne()
+
     if (req.method == "POST") {
 
-        const { name, description, price, discount, imageFilesName } = req.body
+        const { name, description, price, discount, imageFilesName, amount } = req.body
         try {
-            produto.set(null, name, description, price, imageFilesName, discount)
+            produto.set(null, name, description, price, imageFilesName, discount, amount)
         } catch (e) {
             res.status(400).json({ txt: "_id invalido." })
             return
         }
         const insertedProduto = await produto.insertOne()
+        
+        perfil.setEstoque(documentoPerfil?.estoque)
+        await perfil.inserirProdutoEstoque(insertedProduto.insertedId.toString())
+
+        //Apenas para debugar insomnia
+        if(req.rawHeaders.filter((value) => { return value == "insomnia/2022.4.2" })[0] === "insomnia/2022.4.2"){
+            return res.status(200).json({ txt: "Produto criado." })
+        }
 
         const redirectUrl = '/produto/' + insertedProduto.insertedId
         res.redirect(308, redirectUrl)
         
     } else if (req.method == "PUT") {
 
-        const { _id, name, description, price, image, discount } = req.body
+        const { _id, name, description, price, image, discount, amount } = req.body
         if (!_id) {
             res.status(400).json({ txt: "_id não encontrado no body." })
             return
         }
-        produto.set(_id, null, null, null, null, null)
+        produto.set(_id, null, null, null, null, null, null)
         const documentoProduto = await produto.findOne()
         if (!documentoProduto) {
             res.status(400).json({ txt: "Produto não existe." })
             return
         }
-        produto.set(_id, name, description, price, image, discount)
+        produto.set(_id, name, description, price, image, discount, amount)
         await produto.replaceOne()
         res.status(200).json({ txt: "Produto substituído." })
 
     } else if (req.method == "PATCH") {
 
-        const { _id, name, description, price, image, discount } = req.body
+        const { _id, name, description, price, image, discount, amount } = req.body
         if (!_id) {
             res.status(400).json({ txt: "_id não encontrado no body." })
             return
         }
-        produto.set(_id, null, null, null, null, null)
+        produto.set(_id, null, null, null, null, null, null)
         const documentoProduto = await produto.findOne()
         if (!documentoProduto) {
             res.status(400).json({ txt: "Produto não existe." })
             return
         }
-        produto.set(_id, name, description, price, image, discount)
+        produto.set(_id, name, description, price, image, discount, amount)
         await produto.updateOne()
         res.status(200).json({ txt: "Produto atualizado." })
 
@@ -84,7 +95,7 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
             return
         }
         try {
-            produto.set(_id, null, null, null, null, null)
+            produto.set(_id, null, null, null, null, null, null)
         } catch (e) {
             res.status(400).json({ txt: "_id invalido." })
             return
@@ -95,6 +106,10 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
             return
         }
         await produto.deleteOne()
+
+        perfil.setEstoque(documentoPerfil?.estoque)
+        await perfil.excluirProdutoEstoque(_id)
+
         res.status(200).json({ txt: "Produto excluído." })
 
     } else {
