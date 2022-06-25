01e7c6f5fab86d4982da0670df16b1cbabeccf66
Mario Nakazato <marionakazat@gmail.com>
2022-06-24 22:12:04 -0300

    Refatorado forma de identificar se o produto pertence ao perfil

--- pages/api/produto.ts
+++ pages/api/produto.ts
@@ -42,8 +42,8 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
         }
         const insertedProduto = await produto.insertOne()
 
-        perfil.setEstoque(documentoPerfil?.estoque)
-        await perfil.inserirProdutoEstoque(insertedProduto.insertedId.toString())
+        /*perfil.setEstoque(documentoPerfil?.estoque)
+        await perfil.inserirProdutoEstoque(insertedProduto.insertedId.toString())*/
 
         /*//Apenas para debugar insomnia
         if (req.rawHeaders.filter((value) => { return value == "insomnia/2022.4.2" })[0] === "insomnia/2022.4.2") {
@@ -60,16 +60,21 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
             return res.status(400).json({ txt: "_id não encontrado no body." })
         }
 
-        perfil.setEstoque(documentoPerfil?.estoque)
+        /*perfil.setEstoque(documentoPerfil?.estoque)
         if (perfil.getProdutoEstoque(_id) == -1) {
             return res.status(400).json({ txt: "Produto não pertence ao perfil." })
-        }
+        }*/
 
         produto.set(_id, null, null, null, null, null, null, null)
         const documentoProduto = await produto.findOne()
         if (!documentoProduto) {
             return res.status(400).json({ txt: "Produto não existe." })
         }
+
+        if (documentoProduto._idPerfil !== documentoPerfil?._id.toString()) {
+            return res.status(400).json({ txt: "Produto não pertence ao perfil." })
+        }
+
         produto.set(_id, name, description, price, image, discount, amount, _idPerfil)
         await produto.replaceOne()
         res.status(200).json({ txt: "Produto substituído." })
@@ -81,16 +86,21 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
             return res.status(400).json({ txt: "_id não encontrado no body." })
         }
 
-        perfil.setEstoque(documentoPerfil?.estoque)
+        /*perfil.setEstoque(documentoPerfil?.estoque)
         if (perfil.getProdutoEstoque(_id) == -1) {
             return res.status(400).json({ txt: "Produto não pertence ao perfil." })
-        }
+        }*/
 
         produto.set(_id, null, null, null, null, null, null, null)
         const documentoProduto = await produto.findOne()
         if (!documentoProduto) {
             return res.status(400).json({ txt: "Produto não existe." })
         }
+
+        if (documentoProduto._idPerfil !== documentoPerfil?._id.toString()) {
+            return res.status(400).json({ txt: "Produto não pertence ao perfil." })
+        }
+
         produto.set(_id, name, description, price, image, discount, amount, _idPerfil)
         await produto.updateOne()
         res.status(200).json({ txt: "Produto atualizado." })
@@ -105,20 +115,25 @@ export default async function apiProduto(req: NextApiRequest, res: NextApiRespon
             produto.set(_id, null, null, null, null, null, null, null)
         } catch (e) {
             return res.status(400).json({ txt: "_id invalido." })
-            
+
         }
         const documentoProduto = await produto.findOne()
         if (!documentoProduto) {
             return res.status(400).json({ txt: "Produto não existe." })
         }
 
-        perfil.setEstoque(documentoPerfil?.estoque)
-        if (perfil.getProdutoEstoque(_id) == -1) {
+
+        if (documentoProduto._idPerfil !== documentoPerfil?._id.toString()) {
             return res.status(400).json({ txt: "Produto não pertence ao perfil." })
         }
 
+        /*perfil.setEstoque(documentoPerfil?.estoque)
+        if (perfil.getProdutoEstoque(_id) == -1) {
+            return res.status(400).json({ txt: "Produto não pertence ao perfil." })
+        }*/
+
         await produto.deleteOne()
-        await perfil.excluirProdutoEstoque(_id)
+        //await perfil.excluirProdutoEstoque(_id)
 
         res.status(200).json({ txt: "Produto excluído." })
 
