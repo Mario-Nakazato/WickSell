ebd5e3ddcf106310a2ee72ce54a3ad3f09e9d5df
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-07-01 01:52:10 -0300

    Style Carrinho

--- components/CartView.tsx
+++ components/CartView.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import styles from '../styles/Carrinho.module.css'
-export default function CartViewer({ props, transaction, session }: { props: any, transaction: any, session: any }, index: number) {
-    const [onAwait, setOnAwait] = useState(false)
+import { brlMonetary } from '../utils/valuesUtils'
+export default function CartViewer({ props, transaction, session, isHistory, total, setTotal, onAwait, setOnAwait }: { props: any, transaction: any, session: any, isHistory?: any, total: number, setTotal: any, onAwait: boolean, setOnAwait: any }, index: number) {
     const [exist, setExist] = useState(true)
     if (props.props) props = props.props
     if (exist) {
@@ -13,29 +13,29 @@ export default function CartViewer({ props, transaction, session }: { props: any
                         <h1>{props.produto.name}</h1>
                         <p>{props.produto.description}</p>
                     </div>
-                    <div>
-                        <div className={styles.QuantityContainers}>
+                    <div className={styles.Controls}>
+                        {!isHistory || isHistory === false ? <div className={styles.QuantityContainers}>
                             <div className={styles.QuantityControl}>
                                 <button className={styles.Minus} onClick={async () => {
                                     if (props.produto && session) {
-                                        await buyButtonHandler(transaction, props.produto, session, -1, setOnAwait, setExist)
+                                        await buyButtonHandler(transaction, props.produto, session, -1, setOnAwait, setExist, setTotal, total)
                                     }
                                 }} disabled={onAwait}>&minus;</button>
                                 <h1>{props.quantidade}</h1>
                                 <button className={styles.Plus} onClick={async () => {
                                     if (props.produto && session) {
-                                        await buyButtonHandler(transaction, props.produto, session, +1, setOnAwait, setExist)
+                                        await buyButtonHandler(transaction, props.produto, session, +1, setOnAwait, setExist, setTotal, total)
                                     }
                                 }} disabled={onAwait}>&#43;</button>
                             </div>
                             <button className={styles.Trash} onClick={async () => {
                                 if (props.produto && session) {
-                                    await deleteButtonHandler(transaction, props.produto, session, setOnAwait, setExist)
+                                    await deleteButtonHandler(transaction, props.produto, session, setOnAwait, setExist, setTotal, total)
                                 }
                             }} disabled={onAwait}>&times;</button>
-                        </div>
-                        <h4>{props.produto.price}</h4>
-                        <h3>{props.produto.price * props.quantity || props.produto.price * 5}</h3>
+                        </div> : <></>}
+                        <h4>{brlMonetary(props.produto.price)}</h4>
+                        <h3>{brlMonetary((Number(props.produto.price) - Number(props.produto.price) * Number(props.produto.discount) / 100) * Number(props.quantidade))}</h3>
                     </div>
                 </div>
                 <br></br>
@@ -46,25 +46,29 @@ export default function CartViewer({ props, transaction, session }: { props: any
     }
 }
 
-async function buyButtonHandler(transaction: any, data: any, session: any, number: number, onAwaitFc: Function, existFc: Function) {
+async function buyButtonHandler(transaction: any, data: any, session: any, number: number, onAwaitFc: Function, existFc: Function, setTotal: Function, total: number) {
     const control = document.getElementsByClassName(styles.QuantityContainers)[0]
     onAwaitFc(true)
-    console.log(control)
+    var value = 0
+    var qtd = 0
+    var carrinho: any = []
     if (transaction && transaction.carrinho.length > 0) {
         var stocked = false
         transaction.carrinho.forEach((item: any, index: number) => {
             if (item.produto._id === data._id) {
                 item.quantidade += number
+                value = (Number(item.produto.price) - Number(item.produto.price) * Number(item.produto.discount) / 100) * number
                 stocked = true
-            }
-            if (item.quantidade <= 0) {
-                transaction.carrinho.pop(index)
-                existFc(false)
+                qtd = item.quantidade
+            } else {
+                carrinho.push(item)
             }
         })
         var dataBody: any
-        if (stocked) {
+        if (stocked && qtd > 0) {
             dataBody = { _id: transaction._id, carrinho: transaction.carrinho }
+        } else if (stocked && qtd <= 0) {
+            dataBody = { _id: transaction._id, carrinho: carrinho }
         }
         await fetch(`/api/transacao`, {
             method: 'PATCH',
@@ -72,26 +76,29 @@ async function buyButtonHandler(transaction: any, data: any, session: any, numbe
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(dataBody)
-        }).then(res => { if (res.status !== 200) alert('Falha ao adicionar ao carrinho'); })
+        }).then(res => { if (res.status !== 200) alert('Falha ao alterar o carrinho'); else { setTotal(total + value); if (qtd <= 0) existFc(false) } })
     }
     control.classList.remove(styles.Loading)
     onAwaitFc(false)
 }
 
-async function deleteButtonHandler(transaction: any, data: any, session: any, onAwaitFc: Function, existFc: Function) {
+async function deleteButtonHandler(transaction: any, data: any, session: any, onAwaitFc: Function, existFc: Function, setTotal: Function, total: number) {
     onAwaitFc(true)
+    var value = 0
+    var carrinho: any = []
     if (transaction && transaction.carrinho.length > 0) {
         var stocked = false
         transaction.carrinho.forEach((item: any, index: number) => {
             if (item.produto._id === data._id) {
-                transaction.carrinho.pop(index)
                 stocked = true
-                existFc(false)
+                value = (Number(item.produto.price) - Number(item.produto.price) * Number(item.produto.discount) / 100) * item.quantidade
+            } else {
+                carrinho.push(item)
             }
         })
         var dataBody: any
         if (stocked) {
-            dataBody = { _id: transaction._id, carrinho: transaction.carrinho }
+            dataBody = { _id: transaction._id, carrinho }
         }
         await fetch(`/api/transacao`, {
             method: 'PATCH',
@@ -99,7 +106,7 @@ async function deleteButtonHandler(transaction: any, data: any, session: any, on
                 'Content-Type': 'application/json'
             },
             body: JSON.stringify(dataBody)
-        }).then(res => { if (res.status !== 200) alert('Falha ao adicionar ao carrinho'); })
+        }).then(res => { if (res.status !== 200) alert('Falha ao deletar item no carrinho'); else { setTotal(total - value); existFc(false) } })
     }
     onAwaitFc(false)
 }
