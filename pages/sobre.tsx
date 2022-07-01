938230fc4bf632a8beac287569319b9bcb79e69f
Bruno Brandão Silva <72681281+Bruno-Brandao-Silva@users.noreply.github.com>
2022-07-01 11:43:40 -0300

    Sobre

--- pages/sobre.tsx
+++ pages/sobre.tsx
@@ -1,9 +1,11 @@
 export default function Sobre() {
     return <>
-        <div className="container">
-            <h1>Sobre</h1>
+        <div style={{ marginLeft: '10rem', marginRight: '10rem' }}>
+            <h1>WickSell</h1>
+            <div style={{ marginLeft: '2rem', marginRight: '2rem' }}>
             <h2>Sobre o site</h2>
             <p> O site é um projeto desenvolvido para ajudar a venda de produtos de forma rápida e eficiente.</p>
+            </div>
         </div>
     </>
 }
